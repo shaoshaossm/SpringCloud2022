e179f96b6a77e4d4208eaf0f92e38265742d751b
shaoshao <1600767556@qq.com>
2022-03-15 19:26:17 +0800

    SpringCloud eureka provider payment 8002

---
+++ cloud-provider-payment8002/src/main/java/com/ssm/springcloud/PaymentMain8002.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/13 13:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
