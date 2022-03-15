a6c4cdcf9bcc6d3716997bef30418ce7ff5297b8
shaoshao <1600767556@qq.com>
2022-03-15 19:23:52 +0800

    SpringCloud eureka server 7001

---
+++ cloud-eureka-server7001/src/main/java/com/ssm/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/14 19:31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
