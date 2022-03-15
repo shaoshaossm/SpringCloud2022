cd30721909563578800b0c237f4862cdd35c2e48
shaoshao <1600767556@qq.com>
2022-03-15 19:22:44 +0800

    SpringCloud consumer order

---
+++ cloud-consumer-order80/src/main/java/com/ssm/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/13 13:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/ssm/springcloud/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.ssm.springcloud.controller;
+
+import com.ssm.springcloud.entities.CommonResult;
+import com.ssm.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/13 18:11
+ */
+
+@RestController
+@Slf4j
+public class OrderController {
+//    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE"; //服务名称
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public ResponseEntity<CommonResult> create(Payment payment) {
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/get/{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public CommonResult getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+
+}
