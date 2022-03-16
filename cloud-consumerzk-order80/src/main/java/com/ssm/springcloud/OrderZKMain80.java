75ebe6bc0d927f66b6d7d5795deec2fa6ae694ce
shaoshao <1600767556@qq.com>
2022-03-16 21:12:14 +0800

    SpringCloud zookeeper consumer

---
+++ cloud-consumerzk-order80/src/main/java/com/ssm/springcloud/OrderZKMain80.java
@@ -0,0 +1,16 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/13 13:40
+ */
+@SpringBootApplication
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
