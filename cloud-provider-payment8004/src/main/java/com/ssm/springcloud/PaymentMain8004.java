12f6b970db34f720a8701a2004677e35240e58c6
shaoshao <1600767556@qq.com>
2022-03-16 21:11:45 +0800

    SpringCloud zookeeper provider

---
+++ cloud-provider-payment8004/src/main/java/com/ssm/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/13 13:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
