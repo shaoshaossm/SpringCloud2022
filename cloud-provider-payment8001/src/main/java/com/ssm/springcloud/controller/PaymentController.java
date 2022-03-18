6b5e991a99680e6336c482dc321800eb28da871a
shaoshao <1600767556@qq.com>
2022-03-18 20:03:36 +0800

    SpringCloud OpenFeign timeout

--- cloud-provider-payment8001/src/main/java/com/ssm/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/ssm/springcloud/controller/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author shaoshao
@@ -62,8 +63,15 @@ public class PaymentController {
         }
         return this.discoveryClient;
 
-
     }
-
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
+    }
 
 }
