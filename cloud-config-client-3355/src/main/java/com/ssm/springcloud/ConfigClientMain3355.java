4a696109a3af5ed456376dd6a8162469ce35cd65
shaoshao <1600767556@qq.com>
2022-03-21 20:38:32 +0800

    SpringCloud config client

---
+++ cloud-config-client-3355/src/main/java/com/ssm/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/21 19:57
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/ssm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.ssm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/21 19:58
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
+
