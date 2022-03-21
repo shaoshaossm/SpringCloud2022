f9bfc76c9ca6c892ee0a85e51c3013120035d153
shaoshao <1600767556@qq.com>
2022-03-21 20:38:06 +0800

    SpringCloud config center

---
+++ cloud-config-center-3344/src/main/java/com/ssm/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/21 19:18
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
