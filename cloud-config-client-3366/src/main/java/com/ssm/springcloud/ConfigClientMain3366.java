be35d95edb5a60d51b3cba27a9b767eba5a7404c
shaoshao <1600767556@qq.com>
2022-03-22 22:28:12 +0800

    SpringCloud config client3366

---
+++ cloud-config-client-3366/src/main/java/com/ssm/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,21 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/22 19:17
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
+
---
+++ cloud-config-client-3366/src/main/java/com/ssm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,30 @@
+package com.ssm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/22 19:17
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+}
+
