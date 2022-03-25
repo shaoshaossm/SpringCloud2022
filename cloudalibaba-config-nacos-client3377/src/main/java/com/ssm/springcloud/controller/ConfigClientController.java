330faa23908b293abf9820a699cad52992593c3b
shaoshao <1600767556@qq.com>
2022-03-25 21:18:47 +0800

    SpringCloudAlbaba Config Nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/ssm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.ssm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/24 12:04
+ */
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
