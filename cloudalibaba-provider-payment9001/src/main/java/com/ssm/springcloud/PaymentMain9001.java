b6d968691c1090ad662c593951c4bc33fc01fe58
shaoshao <1600767556@qq.com>
2022-03-25 21:20:18 +0800

    SpringCloudAlibaba provider 9001

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/ssm/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/24 10:11
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
