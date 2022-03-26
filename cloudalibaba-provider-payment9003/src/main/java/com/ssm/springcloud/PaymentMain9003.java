8d766523df98bb92aa92f2cd6b81ce1eee7b1d71
shaoshao <1600767556@qq.com>
2022-03-26 18:42:44 +0800

    SpringCloudAlibaba provider9003

---
+++ cloudalibaba-provider-payment9003/src/main/java/com/ssm/springcloud/PaymentMain9003.java
@@ -0,0 +1,18 @@
+package com.ssm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/3/26 14:46
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
